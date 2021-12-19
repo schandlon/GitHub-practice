2537a7b921529a9293d2c04c01d31c7d0fb106ab
schandlon <95983113+schandlon@users.noreply.github.com>
2021-12-18 17:13:30 -0800

    added weather icon image in html and changing icon attribute in js

--- src/script.js
+++ src/script.js
@@ -38,6 +38,12 @@ function showWeatherConditions(response) {
 
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
