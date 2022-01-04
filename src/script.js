ca3bea78a690024133492366838ee60eef29f98d
schandlon <95983113+schandlon@users.noreply.github.com>
2022-01-03 17:38:33 -0800

    Rounded max and min temperatures

--- src/script.js
+++ src/script.js
@@ -23,6 +23,14 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
@@ -34,16 +42,20 @@ function displayForecast(response) {
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${forecastDay.dt}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img 
-        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" 
         alt="" 
         width="42"
     />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-max"> ${forecastDay.temp.max}° </
-          span>
-          <span class="weather-forecast-min"> ${forecastDay.temp.min}° </
+          <span class="weather-forecast-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span 
+          class="weather-forecast-min"> ${Math.round(forecastDay.temp.min)}° </
           span>
         </div>
       </div>
